6fd810ff8cdeccc9ece4b5fccfe5fa61188b8512
ThatAgricBoy <samueljohn3999@gmail.com>
2023-06-23 21:37:39 -0700

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+# installs the package puppet-lint
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# create a manifest that kills a process named killmenow
+# using pkill with puppet
+
+exec { 'pkill':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin',
+}
+
